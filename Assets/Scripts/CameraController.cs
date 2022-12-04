11735d0ce2dc7fbfb980e998c81246363ba7ab67
Azad Kshitij <azadkshitij08302001@gmail.com>
2022-12-04 17:01:45 +0530

    Try new model in scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,30 +17,47 @@ public class CameraController : MonoBehaviour
     private float currentY = 0.0f;
     public float sensivity = 4.0f;
 
+    private bool hasPlayer = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
 
+        
+        
 
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        if(Player)
+        {
+            currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+            currentY -= Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
 
-        currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
-        currentY -= Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+            currentY = Mathf.Clamp(currentY, YMin, YMax);
 
-        currentY = Mathf.Clamp(currentY, YMin, YMax);
+            Vector3 Direction = new Vector3(0, 0, -distance);
+            Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+            transform.position = lookAt.position + rotation * Direction;
 
-        Vector3 Direction = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        transform.position = lookAt.position + rotation * Direction;
+            transform.LookAt(lookAt.position);
+        }
+        /*else
+        {
+            try
+            {
 
-        transform.LookAt(lookAt.position);
+            }
+            catch (System.Exception)
+            {
 
+                throw;
+            }
+        }*/
 
-
+        
     }
 }
