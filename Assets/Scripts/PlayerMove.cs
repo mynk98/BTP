0e1222367648fdc8d17ee844dae0a513c95560dd
Mayank Shekhar <mayankshekhar20142082@gmail.com>
2022-12-09 04:54:17 +0530

    Third person controller with animation in city scene

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,18 +6,32 @@ public class PlayerMove : MonoBehaviour
 {
 
 
-    CharacterController Controller;
+    CharacterController controller;
 
-    public float Speed;
-
+    public float speed;
+    float Horizontal;
+    float Vertical;
     public Transform Cam;
 
+    public Animator animator;
+
+    [SerializeField] float rotationSmoothTime;
+    float currentAngle;
+    float currentAngleVelocity;
+
+    [Header("Gravity")]
+    float gravity = 9.8f;
+    float gravityMultiplier = 2;
+    float groundedGravity = -0.5f;
+    float jumpHeight = 3f;
+    float velocityY = 0;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        Controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 
     }
 
@@ -25,28 +39,40 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
 
-        float Horizontal = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-        float Vertical = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        HandleMovement();
 
-        Vector3 Movement = Cam.transform.right * Horizontal + Cam.transform.forward * Vertical;
-        Movement.y = 0f;
+        //HandleGravityAndJump();
 
 
+    }
 
-        Controller.Move(Movement);
-
-        if (Movement.magnitude != 0f)
+    private void HandleMovement()
+    {
+        //capturing Input from Player
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (movement.magnitude >= 0.1f)
         {
-            transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Cam.GetComponent<CameraController>().sensivity * Time.deltaTime);
-
-
-            Quaternion CamRotation = Cam.rotation;
-            CamRotation.x = 0f;
-            CamRotation.z = 0f;
-
-            transform.rotation = Quaternion.Lerp(transform.rotation, CamRotation, 0.1f);
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + Cam.transform.eulerAngles.y;
+            currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentAngleVelocity, rotationSmoothTime);
+            transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+            Vector3 rotatedMovement = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(rotatedMovement * speed * Time.deltaTime);
+            animator.SetBool("isRunning", true);
 
         }
+        else animator.SetBool("isRunning", false);
+    }
+
+    void HandleGravityAndJump()
+    {
+        if (controller.isGrounded && velocityY < 0f) velocityY = groundedGravity;
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocityY = Mathf.Sqrt(jumpHeight * 2f * gravity);
+            print("jumping");
+        }
+        velocityY -= gravity * gravityMultiplier * Time.deltaTime;
+        controller.Move(Vector3.up * velocityY * Time.deltaTime);
     }
 
 }
